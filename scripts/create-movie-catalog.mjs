1970ade66bac854e3f7b33c159a932d99258ee88
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-08-19 20:06:16 -0400

    Create movie catalog hardcoding script

---
+++ scripts/create-movie-catalog.mjs
@@ -0,0 +1,67 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { loadEnvFile } from 'node:process';
+
+loadEnvFile(fileURLToPath(new URL('../.env', import.meta.url)));
+const { PUBLIC_OMDB_API_KEY } = process.env;
+
+const inputFile = fileURLToPath(new URL('../src/lib/assets/movie-urls.txt', import.meta.url));
+const outputFile = fileURLToPath(new URL('../src/lib/assets/movie-catalog.json', import.meta.url));
+
+async function getVideoTitle(url) {
+  const endpoint = new URL("https://youtube.com/oembed");
+  endpoint.searchParams.append("url", url);
+  endpoint.searchParams.append("format", "json");
+
+  const response = await fetch(endpoint);
+  if (!response.ok) return null;
+  const data = await response.json();
+  const title = data.title;
+
+  // Check if using title-year format (ex.: Cape Fear (1991))
+  const match = title.match(/^(.+?)\s+\((\d{4})\)$/)
+  if (match) return match[1];
+
+  return title;
+}
+
+async function getOmdbData(title) {
+  const endpoint = new URL("https://www.omdbapi.com/");
+  endpoint.searchParams.append("apikey", PUBLIC_OMDB_API_KEY);
+  endpoint.searchParams.append("t", title);
+
+  const response = await fetch(endpoint);
+  if (!response.ok) return null;
+  const data = await response.json();
+  return data;
+}
+
+async function getVideoMetadata(url) {
+  const title = await getVideoTitle(url);
+  if (!title) return null;
+  const omdbData = await getOmdbData(title);
+  if (!omdbData) return null;
+  return {
+    title: title,
+    year: omdbData.Year,
+    imdbId: omdbData.imdbID,
+    poster: omdbData.Poster,
+    description: omdbData.Plot,
+    watchUrl: url,
+    rating: omdbData.imdbRating,
+  };
+}
+
+export async function createMovieCatalog(movieUrls) {
+  const movies = movieUrls.split("\n");
+  const promises = movies.map((url) => getVideoMetadata(url));
+  let catalog = await Promise.all(promises);
+  catalog = catalog.filter((movie) => !!movie);
+  catalog.sort((a, b) => b.rating - a.rating);
+  return catalog;
+};
+
+const movieUrls = await readFile(inputFile, 'utf8');
+const catalog = await createMovieCatalog(movieUrls);
+await writeFile(outputFile, JSON.stringify(catalog, null, 2), 'utf8');
+console.log(`Wrote ${catalog.length} movies to ${outputFile}`);
