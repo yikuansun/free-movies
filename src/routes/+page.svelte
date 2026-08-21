8f3c94cf8024756d33b946dbc3039929c2e36fb5
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-08-21 16:05:36 -0400

    Implement homepage customization
    Very epic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import movieCatalog from "$lib/assets/movie-catalog.json";
     import MovieCarousel from "$lib/components/MovieCarousel.svelte";
     import { localStore } from "$lib/localStore.svelte";
+    import { homepageSectionsMap } from "$lib/data/homepageSections";
 
     let movies: any[] = $state([]);
 
@@ -22,37 +23,15 @@
 </script>
 
 <div class="p-10">
-    {#if watchlist.length > 0}
-        <h2 class="text-2xl font-bold mb-2">Watchlist</h2>
-        <MovieCarousel movies={watchlist.map((url) => movies.find((movie) => movie.watchUrl === url))} />
-        <div class="divider"></div>
-    {/if}
-    {#if favorites.length > 0}
-        <h2 class="text-2xl font-bold mb-2">Favorites</h2>
-        <MovieCarousel movies={favorites.map((url) => movies.find((movie) => movie.watchUrl === url))} />
-        <div class="divider"></div>
-    {/if}
-
-    <h2 class="text-2xl font-bold mb-2">Critically-Acclaimed</h2>
-    <MovieCarousel movies={topRatedMovies} />
-
-    <div class="divider"></div>
-
-    <h2 class="text-2xl font-bold mb-2">Crime Movies</h2>
-    <MovieCarousel movies={crimeMovies} />
-
-    <div class="divider"></div>
-
-    <h2 class="text-2xl font-bold mb-2">Dramas</h2>
-    <MovieCarousel movies={dramaMovies} />
-
-    <div class="divider"></div>
-
-    <h2 class="text-2xl font-bold mb-2">Action Movies</h2>
-    <MovieCarousel movies={actionMovies} />
-
-    <div class="divider"></div>
-
-    <h2 class="text-2xl font-bold mb-2">Science Fiction</h2>
-    <MovieCarousel movies={scifiMovies} />
+    {#each localStore.homepageLayout as homepageSectionId, i (homepageSectionId)}
+        {@const sectionTitle = homepageSectionsMap[homepageSectionId].displayName}
+        {@const sectionMovies = movies.filter(homepageSectionsMap[homepageSectionId].filter)}
+        {#if sectionMovies.length > 0}
+            <h2 class="text-2xl font-bold mb-2">{sectionTitle}</h2>
+            <MovieCarousel movies={sectionMovies} />
+            {#if i < localStore.homepageLayout.length - 1}
+                <div class="divider"></div>
+            {/if}
+        {/if}
+    {/each}
 </div>
